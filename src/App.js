b2061c155fd0ae77ffcc499a5105864f9627d12c
Ramiz Azhar <ramizbrain@gmail.com>
2023-01-28 11:13:35 +0700

    configure react PDF with custom styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ExampleImage from "./assets/example-image.jpg";
+import Logo from "./assets/logo.png";
 import "./App.css";
 
 import {
@@ -7,17 +7,49 @@ import {
   Page,
   Text,
   View,
-  PDFDownloadLink,
   Image,
+  StyleSheet,
+  PDFViewer,
 } from "@react-pdf/renderer";
+
+const st = StyleSheet.create({
+  page: {
+    fontSize: 10,
+    padding: "20px 40px",
+  },
+  renderOnDOM: {
+    width: 1440,
+    height: "100vh",
+  },
+  logo: {
+    width: 150,
+  },
+  flex: {
+    display: "flex",
+  },
+  flexCol: {
+    flexDirection: "col",
+  },
+  flexRow: {
+    flexDirection: "row",
+  },
+  justifyBetween: {
+    justifyContent: "space-between",
+  },
+  p4: {
+    padding: 4,
+  },
+});
 
 const InvoiceDocument = () => {
   return (
     <Document>
-      <Page size="A4">
+      <Page size="A4" style={[st.page]}>
         <View>
-          <Text>Section #1</Text>
-          <Image src={ExampleImage}></Image>
+          <Image src={Logo} style={[st.logo]}></Image>
+          <View>
+            <Text>Halo semua</Text>
+          </View>
         </View>
       </Page>
     </Document>
@@ -26,11 +58,11 @@ const InvoiceDocument = () => {
 
 const App = () => {
   return (
-    <PDFDownloadLink document={<InvoiceDocument />} fileName="invoice.pdf">
-      {({ blob, url, loading, error }) =>
-        loading ? "Loading document..." : "Download now!"
-      }
-    </PDFDownloadLink>
+    <div>
+      <PDFViewer style={st.renderOnDOM}>
+        <InvoiceDocument />
+      </PDFViewer>
+    </div>
   );
 };
 
---
+++ src/pdfStyles.js
@@ -0,0 +1,30 @@
+export const styles = {
+  page: {
+    fontSize: 10,
+    padding: "20px 40px",
+  },
+  renderOnDOM: {
+    width: 1440,
+    height: "100vh",
+  },
+  logo: {
+    width: 150,
+  },
+  flex: {
+    display: "flex",
+  },
+  flexCol: {
+    flexDirection: "col",
+  },
+  flexRow: {
+    flexDirection: "row",
+  },
+  justifyBetween: {
+    justifyContent: "space-between",
+  },
+  p4: {
+    padding: 4,
+  },
+};
+
+export default styles;
